a7b042a652edafabab15da38ca223f676aa20f5a
Zach Snyder <zachsnyder3@gmail.com>
2015-12-22 15:51:27 -0700

    refactoring subnav to projects_base.js, adding active icon fade in/out loop

---
+++ projects/static/projects/projects_base.js
@@ -0,0 +1,62 @@
+var TestScript = (function() {
+	// Not run during QUnit test
+	function main() {
+		var subNavID = "project-subnav";
+		var subnavDelay = 300;
+		var subnavDuration = 700;
+		var activeIconFadeDuration = 1000;
+		subNavActiveLink(activeIconFadeDuration);
+		slideSubNavDown(subNavID, subnavDelay, subnavDuration);
+	}
+	
+	// Causes the .subnav-link-active-icon to fade in and out
+	// infinitely
+	function subNavActiveLink(duration) {
+		function infiniteFade() {
+			$(this).fadeOut(duration, function() {
+				$(this).fadeIn(duration, infiniteFade);
+			});
+		}
+		setTimeout(function() {
+			$('.subnav-link').each(function() {
+				if ($(this).parent().hasClass('active')) {
+					console.log(this);
+					$(this).
+						children('.subnav-link-active-icon').
+						fadeIn(duration, infiniteFade);
+				}
+			});
+		}, 0);
+	}
+	
+	// Slide the subnav down 
+	function slideSubNavDown(subNavID, delay, duration) {
+		setTimeout(function() {
+			$('#' + subNavID).slideDown(duration, function() {});
+		}, delay);
+	}
+	
+	//  If not QUnit test, run main().
+	//  If QUnit test, export the testable functions.
+	try {
+		if (QUnit) {
+			// Export functions for testing...
+			var ProjectsBase = {};
+			ProjectsBase.slideSubNavDown = slideSubNavDown;
+			ProjectsBase.subNavActiveLink = subNavActiveLink;
+	
+			return ProjectsBase;
+		}
+	} catch (e) {
+		console.log("caught an error:");
+		console.log(e);
+		if (e instanceof ReferenceError) {
+			$(document).ready(function() {
+				main();
+			});
+		} else {
+			throw e;
+		}
+		
+	}
+})();
