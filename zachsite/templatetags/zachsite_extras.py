6847bb370be6d0b85b457ea95fc4aa6805b9ee8d
Zach Snyder <zachsnyder3@gmail.com>
2016-01-06 19:10:44 -0700

    adding addlink filter

--- zachsite/templatetags/zachsite_extras.py
+++ zachsite/templatetags/zachsite_extras.py
@@ -1,14 +1,40 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.utils.html import escape
 
 # The register object
 register = template.Library()
 
 def addbreaks(value):
 	"""
-	Replaces 'paragraph-break' in value with '<br>'.
+	Replaces ' |PARAGRAPH-BREAK| ' in value with '<br><br>'.
 	"""
-	return mark_safe(value.replace(' <PARAGRAPH-BREAK> ', '<br><br>'))
+	output = escape(value)
+	return mark_safe(output.replace(' |PARAGRAPH-BREAK| ', '<br><br>'))
+
+def nobreaks(value):
+	"""
+	Replaces ' |PARAGRAPH-BREAK| ' in value with ' '.
+	"""
+	output = escape(value)
+	return mark_safe(output.replace(' |PARAGRAPH-BREAK| ', ' '))
+
+def addlinks(value):
+	"""
+	Replaces '|LINK=href%@%link_text|' in value with 
+	'<a href="href">link_text</a>'.
+	"""
+	output = escape(value)
+	split_text = output.split("|")
+	for i in range(len(split_text)):
+		if split_text[i].find("LINK=") == -1:
+			continue
+		else:
+			split_text[i] = split_text[i].replace("LINK=", '')
+			href_linktext = split_text[i].split("%@%")
+			split_text[i] = "<a href=\"{}\">{}</a>".format(href_linktext[0], href_linktext[1])
+	processed = ''.join(split_text)
+	return mark_safe(processed)
 
 def formatpython(value):
 	"""
@@ -48,7 +74,7 @@ def formatpython(value):
 		'yield'
 	]
 
-	value = "<pre><div>" + value + "</div></pre>"
+	value = "<pre><div>" + escape(value) + "</div></pre>"
 	
 	# wrap keywords in span.keyword
 	for keyword in keywords:
@@ -59,4 +85,6 @@ def formatpython(value):
 
 
 register.filter('addbreaks', addbreaks)
+register.filter('nobreaks', nobreaks)
+register.filter('addlinks', addlinks)
 register.filter('formatpython', formatpython)
