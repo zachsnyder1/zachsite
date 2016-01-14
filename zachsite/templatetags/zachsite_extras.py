6e70df741959c3331ce63887829356d43e450300
Zach Snyder <zachsnyder3@gmail.com>
2016-01-14 13:59:06 -0700

    renaming AudioIO to signalhook; first draft signalhook readme content; partial draft signalhook docs content; formatpython filter now decorates imports, function names, and variable names;

--- zachsite/templatetags/zachsite_extras.py
+++ zachsite/templatetags/zachsite_extras.py
@@ -26,6 +26,7 @@ def formatpython(value):
 	Formats a code string for proper styling.
 	"""
 	output = None
+	imports, functions, variables = [], [], []
 	keywords = [
 		'and ',
 		'as ',
@@ -60,16 +61,66 @@ def formatpython(value):
 	]
 	
 	safe_input = escape(value)
-	value = "<pre><div>" + safe_input + "</div></pre>"
+	input_lines = safe_input.split('\n')
 	
-	# wrap keywords in span.keyword
-	for keyword in keywords:
-		markedup = '<span class="keyw">{}</span>'.format(keyword)
-		value = value.replace(keyword, markedup)
+	for line in range(len(input_lines)):
+		# don't process one line comments
+		if re.search(r'^[\s]*[#]', input_lines[line]):
+			continue
+		else:
+			pass
+		
+		# add imported entities to imports list
+		isymbol = re.sub(r'from [\w\S]* import ([\w\S]*)[\s]*$',
+						r'\1',
+						input_lines[line])
+		if isymbol != input_lines[line]:
+			imports.append(isymbol.replace('\r', ''))
+		else:
+			pass				
+		
+		# add function names to symbols list
+		fsymbol = re.sub(r'^def ([\w]*)\(.*\):', 
+						r'\1', 
+						input_lines[line])
+		if fsymbol != input_lines[line]:
+			functions.append(fsymbol.replace('\r', ''))
+		else:
+			pass
+		
+		# add var names to symbols list
+		vsymbol = re.sub(r'^([\w]*) = .*',
+						r'\1',
+						input_lines[line])
+		if vsymbol != input_lines[line]:
+			variables.append(vsymbol.replace('\r', ''))
+		else:
+			pass
+		
+		# decorate symbols:
+		for imp in imports:
+			markedup = '<span class="imp">{}</span>'.format(imp)
+			input_lines[line] = input_lines[line].replace(imp, markedup)
+		
+		# decorate symbols:
+		for func in functions:
+			markedup = '<span class="func">{}</span>'.format(func)
+			input_lines[line] = input_lines[line].replace(func, markedup)
+		
+		# decorate symbols:
+		for var in variables:
+			markedup = '<span class="var">{}</span>'.format(var)
+			input_lines[line] = input_lines[line].replace(var, markedup)
+		
+		# decorate keywords:
+		for keyword in keywords:
+			markedup = '<span class="keyw">{}</span>'.format(keyword)
+			input_lines[line] = input_lines[line].replace(keyword, markedup)
 	
-	return mark_safe(value)
-
-
+	output = "<pre><div>" + '\n'.join(input_lines) + "</div></pre>"
+	return mark_safe(output)
+		
+		
 register.filter('no_breaks', no_breaks)
 register.filter('add_breaks', add_breaks)
 register.filter('formatpython', formatpython)
