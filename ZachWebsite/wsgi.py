11c1a45e699a029efa9bb3ddf68429608f34e3fe
Zach Snyder <zachsnyder3@gmail.com>
2016-01-25 17:41:25 -0700

    adding deployment dirs to sys.path

--- ZachWebsite/wsgi.py
+++ ZachWebsite/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/zachsite/site/ZachWebsite')
+sys.path.append('/zachsite/site')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ZachWebsite.settings")
 
