66b088311ebcac619242dc9741cb32a6a040f1cd
Zach Snyder <zachsnyder3@gmail.com>
2016-04-02 17:45:18 -0600

    first selenium tests for geopost home page

--- geopost/selenium_tests/pages.py
+++ geopost/selenium_tests/pages.py
@@ -1,4 +1,7 @@
 from .locators import HomeLocators
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
 	"""
@@ -6,20 +9,42 @@ class BasePage:
 	"""
 	def __init__(self, driver):
 		self.driver = driver
-
-class GeopostHomeAnonymousPage(BasePage):
-	"""
-	Page objects for Geopost home page as anonymous user.
-	"""
-	def is_info_displayed(self):
-		"""
-		Return BOOL indicating whether info modal is displayed.
-		"""
-		info = self.driver.find_element(*HomeLocators.INFO_MODAL)
-		style = info.get_attribute('style')
-		return 'display: block' in style
 	
-	def click_point(self):
+	def verify(self, condition, time=2):
+		"""
+		Return True if condition is verified within time.
+		"""
+		try:
+			WebDriverWait(self.driver, time).until(condition)
+			return True
+		except TimeoutException:
+			return False
+	
+	def absent(self, locator):
+		"""
+		Return True if element is absent, else False.
+		"""
+		try:
+			el = self.driver.find_element(*locator)
+			return False
+		except NoSuchElementException:
+			return True
+
+class GeopostHomePage(BasePage):
+	"""
+	Page objects for Geopost home page.
+	"""
+	# ---------------------------------------------------------------
+	# ---------------------- GENERAL ACTIONS ------------------------
+	# ---------------------------------------------------------------
+	def get_img_src(self):
+		"""
+		Get the src attribute of the entry img element.
+		"""
+		elem = self.driver.find_element(*HomeLocators.ENTRY_IMG)
+		return elem.get_attribute('src')
+	
+	def open_entry(self):
 		"""
 		Click on an entry in the map to display its content.
 		"""
@@ -35,35 +60,7 @@ class GeopostHomeAnonymousPage(BasePage):
 		Close the info modal.
 		"""
 		closebtn = self.driver.find_element(*HomeLocators.CLOSE_BTN)
-	
-	def get_title(self):
-		"""
-		Get the title currently displayed. 
-		""" 
-		elem = self.driver.find_element(*HomeLocators.ENTRY_TITLE)
-		return elem.getText()
-	
-	def get_body(self):
-		"""
-		Get the text body currently displayed.
-		"""
-		elem = self.driver.find_element(*HomeLocators.ENTRY_BODY)
-		return elem.getText()
-	
-	def get_img_src(self):
-		"""
-		Get the src attribute of the entry img element.
-		"""
-		elem = self.driver.find_element(*HomeLocators.ENTRY_IMG)
-		return elem.get_attribute('src')
-	
-	def is_attribution_displayed(self):
-		"""
-		Return BOOL indicating whether attribution on map is displayed.
-		"""
-		attribution = self.driver.find_element(*HomeLocators.ATTRIBUTION)
-		classStr = attribution.get_attribute('class')
-		return 'ol-collapsed' not in classStr
+		closebtn.click()
 	
 	def toggle_attribution(self):
 		"""
@@ -71,9 +68,112 @@ class GeopostHomeAnonymousPage(BasePage):
 		"""
 		attrBtn = self.driver.find_element(*HomeLocators.ATTR_BTN)
 		attrBtn.click()
-
-class GeopostHomeAuthPage(GeopostHomeAnonymousPage):
-	"""
-	Additional methods for Geopost home page as auth'ed user.
-	"""
-	pass
+	
+	# ---------------------------------------------------------------
+	# ------------------- VERIFICATION METHODS ----------------------
+	# ---------------------------------------------------------------
+	def verify_entry_displayed(self):
+		"""
+		Return True if info modal is displayed.
+		"""
+		info = self.driver.find_element(*HomeLocators.INFO_MODAL)
+		return self.verify(EC.visibility_of(info))
+	
+	def verify_entry_hidden(self):
+		"""
+		Return True if info modal is not displayed.
+		"""
+		condition = EC.invisibility_of_element_located(HomeLocators.INFO_MODAL)
+		return self.verify(condition)
+	
+	def verify_title_empty(self):
+		"""
+		Wait for title to be empty. 
+		""" 
+		title = self.driver.find_element(*HomeLocators.ENTRY_TITLE)
+		return self.verify(lambda driver: title.text == '')
+	
+	def verify_title_not_empty(self):
+		"""
+		Wait for title to be not empty.
+		"""
+		title = self.driver.find_element(*HomeLocators.ENTRY_TITLE)
+		return self.verify(lambda driver: title.text != '')
+	
+	def verify_body_empty(self):
+		"""
+		Wait for body to be empty.
+		"""
+		body = self.driver.find_element(*HomeLocators.ENTRY_BODY)
+		return self.verify(lambda driver: body.text == '')
+	
+	def verify_body_not_empty(self):
+		"""
+		Wait for body to be not empty.
+		"""
+		body = self.driver.find_element(*HomeLocators.ENTRY_BODY)
+		return self.verify(lambda driver: body.text != '')
+	
+	def verify_img_load(self):
+		"""
+		Wait for the image to load.
+		"""
+		return self.verify(lambda driver: self.get_img_src() != '', time=8)
+	
+	def verify_attribution_displayed(self):
+		"""
+		Wait for attribution to be displayed.
+		"""
+		attribution = self.driver.find_element(*HomeLocators.ATTRIBUTION)
+		c = 'ol-collapsed' # the class indicating that attr is collapsed
+		condition = lambda driver: c in attribution.get_attribute('class')
+		return self.verify(condition)
+	
+	def verify_attribution_not_displayed(self):
+		"""
+		Wait for attribution to be not displayed.
+		"""
+		attribution = self.driver.find_element(*HomeLocators.ATTRIBUTION)
+		c = 'ol-collapsed' # collapsed class
+		condition = lambda driver: c not in attribution.get_attribute('class')
+		return self.verify(condition)
+	
+	def verify_toolbar_present(self):
+		"""
+		Wait for toolbar to be present.
+		"""
+		condition = EC.presence_of_element_located(HomeLocators.TOOLBAR)
+		return self.verify(condition)
+	
+	def verify_toolbar_absent(self):
+		"""
+		True if toolbar is absent.
+		"""
+		return self.absent(HomeLocators.TOOLBAR)
+	
+	def verify_edit_button_present(self):
+		"""
+		Wait for edit button to be present.
+		"""
+		condition = EC.presence_of_element_located(HomeLocators.EDIT_BTN)
+		return self.verify(condition)
+	
+	def verify_edit_button_absent(self):
+		"""
+		True if edit button is absent.
+		"""
+		return self.absent(HomeLocators.EDIT_BTN)
+	
+	def verify_delete_button_present(self):
+		"""
+		Wait for delete button to be present.
+		"""
+		condition = EC.presence_of_element_located(HomeLocators.DELETE_BTN)
+		return self.verify(condition)
+	
+	def verify_delete_button_absent(self):
+		"""
+		True if delete button is absent.
+		"""
+		return self.absent(HomeLocators.DELETE_BTN)
+	
--- geopost/selenium_tests/test_home_anonymous.py
+++ geopost/selenium_tests/test_home_anonymous.py
@@ -1,6 +1,5 @@
 import unittest
 import os
-import time
 import sys
 PACKAGE_ROOT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), 
@@ -22,7 +21,7 @@ class HomeAnonymousTests(unittest.TestCase):
 		"""
 		self.driver = webdriver.Firefox()
 		self.driver.get('http://127.0.0.1:8000/projects/geopost/')
-		self.page = pages.GeopostHomeAnonymousPage(self.driver)
+		self.page = pages.GeopostHomePage(self.driver)
 	
 	def tearDown(self):
 		"""
@@ -31,11 +30,43 @@ class HomeAnonymousTests(unittest.TestCase):
 		self.driver.close()
 	
 	def test_modal_open_and_close(self):
-		self.assertFalse(self.page.is_info_displayed())
-		self.page.click_point()
-		self.assertTrue(self.page.is_info_displayed())
+		"""
+		Test that the info modal opens when an entry is clicked,
+		and then closes when the close button is clicked.
+		"""
+		self.assertTrue(self.page.verify_entry_hidden())
+		self.page.open_entry()
+		self.assertTrue(self.page.verify_entry_displayed())
 		self.page.dismiss_info()
-		self.assertFalse(self.page.is_info_displayed())
+		self.assertTrue(self.page.verify_entry_hidden())
+	
+	def test_info_displayed(self):
+		"""
+		Test that when an entry is clicked, the title and body
+		elements of the info modal are populated with information,
+		and that a photo is eventually loaded.
+		"""
+		# First, they should have no info:
+		self.assertTrue(self.page.verify_title_empty())
+		self.assertTrue(self.page.verify_body_empty())
+		self.assertFalse(self.page.get_img_src())
+		# Open an entry:
+		self.page.open_entry()
+		# Now the title and body should be present:
+		self.assertTrue(self.page.verify_title_not_empty())
+		self.assertTrue(self.page.verify_body_not_empty())
+		# ...and the image should load in a few seconds:
+		self.assertTrue(self.page.verify_img_load())
+	
+	def test_auth_elements_not_present(self):
+		"""
+		Test that Django correctly omitted the toolbar and edit/delete
+		buttons for anonymous user.
+		"""
+		self.assertTrue(self.page.verify_toolbar_absent())
+		self.assertTrue(self.page.verify_edit_button_absent())
+		self.assertTrue(self.page.verify_delete_button_absent())
+		
 
 if __name__ == '__main__':
 	unittest.main()
