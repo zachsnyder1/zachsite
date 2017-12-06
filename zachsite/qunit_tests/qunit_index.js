85381d34f6599096ece37915c787f3bcb2a017cd
Zach Snyder <zachsnyder3@gmail.com>
2017-12-05 20:35:36 -0800

    index revamp

--- zachsite/qunit_tests/qunit_index.js
+++ zachsite/qunit_tests/qunit_index.js
@@ -1,49 +1,49 @@
 QUnit.module("Test index.js functions");
-QUnit.test("test qaCarUpdate", function(assert) {
+QUnit.test("test pCarUpdate", function(assert) {
 	var fixture = $("#qunit-fixture");
-	var questionCarousel = "question-carousel";
-	var qCarouselInner = "carousel-inner";
-	var answerParagraph = "answer-text";
+	var projectCarousel = "project-carousel";
+	var pCarouselInner = "carousel-inner";
+	var summaryParagraph = "project-summary-text";
 	var done = assert.async();
 	
 	// Make carousel
 	$("<div class=\"carousel slide\" data-ride=\"carousel\"></div>").
-		attr('id', questionCarousel).
+		attr('id', projectCarousel).
 		appendTo(fixture);
 	// Make carousel-inner
 	$("<div class=\"carousel-inner\" role=\"listbox\"></div>").
-		attr('id', qCarouselInner).
-		appendTo($('#' + questionCarousel));
+		attr('id', pCarouselInner).
+		appendTo($('#' + projectCarousel));
 	// Make two items
 	$("<div class=\"item active\"></div>").
 		attr('id', "child1").
-		appendTo($('#' + qCarouselInner)).
+		appendTo($('#' + pCarouselInner)).
 		attr('data-answer', "test1");
 	$("<div class=\"item\"></div>").
 		attr('id', "child2").
-		appendTo($('#' + qCarouselInner)).
+		appendTo($('#' + pCarouselInner)).
 		attr('data-answer', "test2");
 	// Make the answer paragraph
-	$("<p></p>").attr('id', answerParagraph).appendTo(fixture);
+	$("<p></p>").attr('id', summaryParagraph).appendTo(fixture);
 	
-	// Call qaCarUpdate to set event listener and set answerParagraph
+	// Call pCarUpdate to set event listener and set summaryParagraph
 	// to the data-attribute of child1
-	TestScript.qaCarUpdate('#' + questionCarousel, 
-						 '#' + qCarouselInner, 
-						 '#' + answerParagraph);
-	assert.equal($('#' + answerParagraph).text(), 
+	TestScript.pCarUpdate('#' + projectCarousel, 
+						 '#' + pCarouselInner, 
+						 '#' + summaryParagraph);
+	assert.equal($('#' + summaryParagraph).text(), 
 				 "test1", 
 				 "initial text loaded");
 	// On 'slid' event, assert text has been successfully updated:
-	$('#' + questionCarousel).on('slid.bs.carousel', function() {
+	$('#' + projectCarousel).on('slid.bs.carousel', function() {
 		// requeue assertion to assure it follows text update
 		setTimeout(function() {
-			assert.equal($('#' + answerParagraph).text(), 
+			assert.equal($('#' + summaryParagraph).text(), 
 						 "test2", 
 						 "text after slide");
 			done();
 		}, 100);
 	});
 	// Trigger the event
-	$('#' + questionCarousel).carousel('next');
+	$('#' + projectCarousel).carousel('next');
 });
--- zachsite/static/zachsite/index.js
+++ zachsite/static/zachsite/index.js
@@ -3,22 +3,22 @@ var TestScript = (function() {
 	// Not run during QUnit test
 	function main() {
 		"use strict";
-		// Make #question-carousel update #answer-text div on 'slide' 
-		var questionCarousel = "#question-carousel";
-		var qCarouselInner = "#carousel-inner";
-		var answerParagraph = "#answer-text";
-		qaCarUpdate(questionCarousel, qCarouselInner, answerParagraph);
+		// Make #project-carousel update #project-summary-text div on 'slide' 
+		var projectCarousel = "#project-carousel";
+		var pCarouselInner = "#carousel-inner";
+		var summaryParagraph = "#project-summary-text";
+		pCarUpdate(projectCarousel, pCarouselInner, summaryParagraph);
 	}
 	
-	// Make #question-carousel update #answer-text div on 'slide'
-	function qaCarUpdate(questionCarousel, qCarouselInner, answerParagraph) {
-		$(answerParagraph).text($(qCarouselInner).children()[0].dataset.answer);
-		$(questionCarousel).on('slide.bs.carousel', function(e) {
-			fadeTrans(answerParagraph, e.relatedTarget.dataset.answer, 600);
+	// Make #project-carousel update #project-summary-text div on 'slide'
+	function pCarUpdate(projectCarousel, pCarouselInner, summaryParagraph) {
+		$(summaryParagraph).text($(pCarouselInner).children()[0].dataset.answer);
+		$(projectCarousel).on('slide.bs.carousel', function(e) {
+			fadeTrans(summaryParagraph, e.relatedTarget.dataset.answer, 600);
 		});
 	}
 	
-	// Used to provide fade to qaCarUpdate()
+	// Used to provide fade to pCarUpdate()
 	function fadeTrans(selector, newText, duration) {
 		$(selector).fadeOut(duration, function(elem2) {
 			$(selector).text(newText);
@@ -32,7 +32,7 @@ var TestScript = (function() {
 		if (QUnit) {
 			// Export functions for testing...
 			var Index = {};
-			Index.qaCarUpdate = qaCarUpdate;
+			Index.pCarUpdate = pCarUpdate;
 	
 			return Index;
 		}
