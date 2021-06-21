50529966826b037f6eb4665e564d9d71a47d5c1b
zachsnyder1 <zachsnyder3@gmail.com>
2021-06-21 06:20:37 +0000

    randos

--- projects/urls.py
+++ projects/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
         # Web app includes:
-        url(r'^geopost/', include('geopost.urls')),
+        url(r'^geopost/', include('geopost.urls'))
         # Projects app urls:
         url(r'^$', views.projects_home, name='projects_home'),
         url(r'^(?P<project_id>[0-9]+)/(?P<project_slug>[0-9A-Za-z_-]+)/$',
