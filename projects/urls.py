531d5793b2cc050138cc8c914390e6ee817c7846
zachsnyder1 <zachsnyder3@gmail.com>
2021-06-27 01:14:33 +0000

    updating projects urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
         # Web app includes:
-        url(r'^geopost/', include('geopost.urls'))
+        re_path(r'^geopost/', include('geopost.urls')),
         # Projects app urls:
-        url(r'^$', views.projects_home, name='projects_home'),
-        url(r'^(?P<project_id>[0-9]+)/(?P<project_slug>[0-9A-Za-z_-]+)/$',
+        re_path(r'^$', views.projects_home, name='projects_home'),
+        re_path(r'^(?P<project_id>[0-9]+)/(?P<project_slug>[0-9A-Za-z_-]+)/$',
             views.project_about,
             name='project_about'),
-        url(r'^(?P<project_id>[0-9]+)/(?P<project_slug>[0-9A-Za-z_-]+)/docs/$',
+        re_path(r'^(?P<project_id>[0-9]+)/(?P<project_slug>[0-9A-Za-z_-]+)/docs/$',
             views.project_docs,
             name='project_docs')
 ]
