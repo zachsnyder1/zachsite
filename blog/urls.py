298d527cf7b0cca184fd7f670147dd7f38aa42e8
zachsnyder1 <zachsnyder3@gmail.com>
2022-06-19 22:27:36 -0700

    updating url to re_path in blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.blog_entries, name='blog_entries'),
-        url(r'^(?P<entry_uuid>[0-9A-Fa-f-]+)/(?P<entry_slug>[0-9A-Za-z_-]+)/$',
+        re_path(r'^$', views.blog_entries, name='blog_entries'),
+        re_path(r'^(?P<entry_uuid>[0-9A-Fa-f-]+)/(?P<entry_slug>[0-9A-Za-z_-]+)/$',
             views.blog_detail,
             name='blog_detail'),
 ]
